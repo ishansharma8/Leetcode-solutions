31cc0f21fe2e673e1876c61f12d6fc7753a96020
Ishan-sharma8 <80027966+Ishan-sharma8@users.noreply.github.com>
2022-06-24 11:19:12 +0530

    Time: 128 ms (36.14%), Space: 28.4 MB (80.90%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.cpp
+++ 912-sort-an-array/912-sort-an-array.cpp
@@ -1,55 +1,36 @@
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
-        int n=nums.size(); 
-        mergesort(nums,0,n-1);    //making 1 func mergesort
+        int n=nums.size();
+        quicksort(nums,0,n-1);
         return nums;
     }
     
-    void mergesort(vector<int>& nums,int l,int r){    //sorting frm l to r
-        if(l>=r){         //when we hv 1 elemnt ,then left & rht same
-            return;       //doin noth & func returtype void sotyped only return
-        }
-        int mid=l+(r-l)/2;
-        mergesort(nums,l,mid);
-        mergesort(nums,mid+1,r);
-        merge(nums,l,mid,r);   
+    void quicksort(vector<int>& nums,int st,int end){
+        //subarray defined by starting nd ending index
+        if(st>=end){
+            return;
+        } 
+        int pivot_index=partition(nums,st,end);
+        quicksort(nums,st,pivot_index-1); 
+        quicksort(nums,pivot_index+1,end); 
     }
     
-    void merge(vector<int>& nums,int l,int mid,int r){
-        vector<int>tempL,tempR;
-
-        for(int i=l;i<=mid;i++){
-            tempL.push_back(nums[i]);
+    int partition(vector<int>& nums,int st,int end){
+        int random=st+rand()%(end-st);
+        swap(nums[end],nums[random]);
+        int pivot=nums[end];
+        int j=st-1;
+        for(int i=st;i<end;i++){
+            if(nums[i]<pivot){
+                j++;
+            swap(nums[j],nums[i]);
         }
-        for(int i=mid+1;i<=r;i++){
-            tempR.push_back(nums[i]);
-        }
+    }
+        j++;
+        swap(nums[j],nums[end]); //all nos less than last elemnt are upto j
+        return j;
         
-        int tempLP=0,tempRP=0,numsP=l;
-        while(tempLP<tempL.size()&&tempRP<tempR.size()){
-            if(tempL[tempLP]<=tempR[tempRP]){
-                nums[numsP]=tempL[tempLP];
-                tempLP++;
-                numsP++;
-            }
-            else{
-                nums[numsP]=tempR[tempRP];
-                tempRP++;
-                numsP++;   
-            }
-        }
-        
-        while(tempLP<tempL.size()){
-            nums[numsP]=tempL[tempLP];
-            tempLP++;
-            numsP++;
-        }
-        while(tempRP<tempR.size()){
-            nums[numsP]=tempR[tempRP];
-            tempRP++;
-            numsP++;
-        }
-        }
-       
+    }
+    
 };
