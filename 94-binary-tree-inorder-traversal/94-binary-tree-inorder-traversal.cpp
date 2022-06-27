759ca543066621efbf0a11215d1f0f49cbba29cf
Ishan-sharma8 <80027966+Ishan-sharma8@users.noreply.github.com>
2022-06-27 23:01:15 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (40.81%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -12,17 +12,17 @@
 class Solution {
 public:
     void helper(TreeNode* root,vector<int>&ans){
-        if(root==NULL){
-            return;                                 //return vector<int>();
-        }
+        if(root==NULL)return;
+        
         helper(root->left,ans);
         ans.push_back(root->val);
         helper(root->right,ans);
+        
     }
-                
+    
     vector<int> inorderTraversal(TreeNode* root) {
-     vector<int>ans;
-        helper(root,ans);
+        vector<int>ans;
+        helper(root,ans);     
         return ans;
     }
 };
