019f64135a1ef7a816736992ccb93143f4efd11a
Ishan-sharma8 <80027966+Ishan-sharma8@users.noreply.github.com>
2022-06-28 14:37:10 +0530

    Time: 324 ms (25.95%), Space: 10.7 MB (32.27%) - LeetHub

--- 395-longest-substring-with-at-least-k-repeating-characters/395-longest-substring-with-at-least-k-repeating-characters.cpp
+++ 395-longest-substring-with-at-least-k-repeating-characters/395-longest-substring-with-at-least-k-repeating-characters.cpp
@@ -18,15 +18,18 @@ public:
         }
         
         cout<<begin<<" "<<end<<endl;
-        unordered_map<char,int>mp;
-        for(int i=begin;i<=end;i++){
-            mp[s[i]]++;  
+        vector<int>v(26,0);
+        for(int i=begin; i<=end; i++)
+        {
+            int index = s[i] - 'a';
+            v[index]++;
+            
         }
         
        // for(auto i:mp){cout<<i.first<<" "<<i.second<<endl;}
         int ans=end-begin+1;
         for(int j=begin;j<=end;j++){
-            if(mp[s[j]]<k){
+            if(v[s[j]- 'a']<k){
                 int left=helper(s,begin,j-1,k);
                 int right=helper(s,j+1,end,k);
                 ans=max(left,right);
