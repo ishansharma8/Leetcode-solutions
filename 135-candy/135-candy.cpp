e53e1cac12f3e1e2fdd378ec706ac4c65baebc47
Ishan-sharma8 <80027966+Ishan-sharma8@users.noreply.github.com>
2022-07-04 14:43:10 +0530

    Time: 265 ms (5.09%), Space: 18.3 MB (23.59%) - LeetHub

---
+++ 135-candy/135-candy.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n=ratings.size();
+        vector<int>v1(n,1);
+        vector<int>v2(n,1);
+        
+        for(int i=1;i<n;i++){
+            if(ratings[i]>ratings[i-1])
+                v1[i]=v1[i-1]+1;
+        }
+        for(int i=0;i<n;i++){
+            cout<<v1[i]<<" ";
+        }
+        cout<<endl;
+        for(int i=n-2;i>=0;i--){
+            if(ratings[i]>ratings[i+1])
+                v2[i]=v2[i+1]+1;
+        }
+        for(int i=0;i<n;i++){
+            cout<<v2[i]<<" ";
+        }
+        for(int i=0;i<n;i++){
+            v1[i]=max(v1[i],v2[i]);
+        }
+         
+        return accumulate(v1.begin(),v1.end(),0);
+        
+    }
+};
