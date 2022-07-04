859f43e8dc9b2e1e3517ad4ce12df8553d270ff9
Ishan-sharma8 <80027966+Ishan-sharma8@users.noreply.github.com>
2022-07-04 14:43:40 +0530

    Time: 47 ms (21.19%), Space: 18.2 MB (23.59%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -9,17 +9,17 @@ public:
             if(ratings[i]>ratings[i-1])
                 v1[i]=v1[i-1]+1;
         }
-        for(int i=0;i<n;i++){
-            cout<<v1[i]<<" ";
-        }
-        cout<<endl;
+        // for(int i=0;i<n;i++){
+        //     cout<<v1[i]<<" ";
+        // }
+        // cout<<endl;
         for(int i=n-2;i>=0;i--){
             if(ratings[i]>ratings[i+1])
                 v2[i]=v2[i+1]+1;
         }
-        for(int i=0;i<n;i++){
-            cout<<v2[i]<<" ";
-        }
+        // for(int i=0;i<n;i++){
+        //     cout<<v2[i]<<" ";
+        // }
         for(int i=0;i<n;i++){
             v1[i]=max(v1[i],v2[i]);
         }
