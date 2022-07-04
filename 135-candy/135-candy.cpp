d8d4c979715c2b84ec7253cbb3db3567ded0c61c
Ishan-sharma8 <80027966+Ishan-sharma8@users.noreply.github.com>
2022-07-04 15:14:57 +0530

    Time: 43 ms (27.66%), Space: 17.6 MB (84.26%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -3,7 +3,7 @@ public:
     int candy(vector<int>& ratings) {
         int n=ratings.size();
         vector<int>v1(n,1);
-        vector<int>v2(n,1);
+        //vector<int>v2(n,1);
         
         for(int i=1;i<n;i++){
             if(ratings[i]>ratings[i-1])
@@ -14,15 +14,15 @@ public:
             if(ratings[i]>ratings[i+1])
                 
                 v1[i]=max(v1[i+1]+1,v1[i]);
-                //cout<<ratings[i]<<" "<<v1[i]<<" "<<endl;    
-        }
-    
-        
-//         for(int i=0;i<n;i++){
-//             cout<<v1[i];
-//         }
-         
+                   
+        }   
         return accumulate(v1.begin(),v1.end(),0);
         
     }
 };
+//         for(int i=0;i<n;i++){
+//             cout<<v1[i];
+//         }
+
+
+//cout<<ratings[i]<<" "<<v1[i]<<" "<<endl; 
