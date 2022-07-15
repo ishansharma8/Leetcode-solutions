42d990f1e88f4425609c0d8e9f0608ce4a4a30d0
Ishan-sharma8 <80027966+Ishan-sharma8@users.noreply.github.com>
2022-07-15 15:41:52 +0530

    Time: 177 ms (50.68%), Space: 96.1 MB (85.93%) - LeetHub

--- 463-island-perimeter/463-island-perimeter.cpp
+++ 463-island-perimeter/463-island-perimeter.cpp
@@ -1,45 +1,32 @@
 class Solution {
 public:
     int islandPerimeter(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-        queue<pair<int,int>>q;
-        vector<vector<bool>>visited(grid.size(),vector<bool>(grid[0].size(),0));
+        int m = grid.size(); //row
+        int n = grid[0].size(); //column
+        int ans =0;
         
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
-                if(grid[i][j]==1){
-                    q.push(make_pair(i,j));
-                  //  visited[i][j]=1;
-                        break;
-                }
-            }
-        }
-        
-        int ans=0;
-        
-        
-        while(q.size())
-        {
-            pair<int,int>p=q.front();
-            q.pop();
-            int left[4]={1,0,-1,0};
-            int right[4]={0,1,0,-1};
-            
-            if(!visited[p.first][p.second]){
-                ans+=4;
-                for(int i=0;i<4;i++){
-                    int row=left[i]+p.first;
-                    int col=right[i]+p.second;
-                    
-                    if(row>=0 && col>=0 && row<n && col<m && grid[row][col]==1){
-                        ans--;
-                        q.push(make_pair(row,col));
-                    }
-                }
-                visited[p.first][p.second]=1;
-            }
-
+        for(int i=0;i<m;i++){
+             for(int j = 0;j<n;j++){
+                 if(grid[i][j] == 1){
+                     ans+=4; 
+                     
+                     // if down neighbour is there
+                     if( i+1 <m  && grid[i+1][j] == 1  ){
+                         ans--;
+                     }  
+                     if( i-1 >=0  && grid[i-1][j] == 1  ){
+                         ans--;
+                     }  
+                     if(   j-1>=0  && grid[i][j-1] == 1)  // if left neighbour
+                     {   
+                         ans--;
+                     }
+                      if(  j+1<n && grid[i][j+1] == 1 ) {//if right neighbour
+                          ans--;
+                      }
+    
+                 }
+             }
         }
         return ans;
     }
