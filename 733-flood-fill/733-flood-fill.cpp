1a7053aba5a5c18cae7183370107e6bf098e4949
Ishan-sharma8 <80027966+Ishan-sharma8@users.noreply.github.com>
2022-07-16 09:24:49 +0530

    Time: 20 ms (26.50%), Space: 14.3 MB (26.16%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -13,25 +13,25 @@ public:
         while(!q.empty()){
             pair<int,int>p=q.front();
             q.pop();
+           
             int x=p.first;
             int y=p.second;
+            visited[x][y]=1;
+         
             int left[4]={1,0,-1,0};
             int right[4]={0,1,0,-1};
+            for(int i=0;i<4;i++){
+            int row=left[i]+x;
+            int col=right[i]+y;
             
-            
-            
-            
-            if(visited[x][y]!=1){
-                for(int i=0;i<4;i++){
-                int row=left[i]+p.first;
-                int col=right[i]+p.second;
-                if(row>=0 && col>=0 && row<n && col<m && image[row][col]==image[x][y]){
+    if(row>=0 && col>=0 && row<n && col<m && visited[row][col]==0 && image[row][col]==image[x][y]){
                 q.push({row,col});
+                visited[row][col]=1;
             }
-        }
-                visited[x][y]=1;
+                cout<<q.size()<<" ";
+            }
+                
                 image[x][y]=color;
-            }
         }
     
          return image;
