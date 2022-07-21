a4c10fef71dbe35e630492506cda21acfa0c2279
Ishan-sharma8 <80027966+Ishan-sharma8@users.noreply.github.com>
2022-07-21 18:36:10 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (24.74%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,27 +1,15 @@
 class Solution {
 public:
-    int robfrom(int currentindex,vector<int>&wealth,vector<int>&dp,int n){
-        
-        if(currentindex>=n)
-            return 0;
-        if(dp[currentindex]!=-1){
-            return dp[currentindex];
-        }
-        
-        int stealcurrent=wealth[currentindex]+robfrom(currentindex+2,wealth,dp,n);
-        int skipcurrent=robfrom(currentindex+1,wealth,dp,n);
-        
-        int ans=max(stealcurrent,skipcurrent);
-        dp[currentindex]=ans;
-        return ans;
-    }
     int rob(vector<int>& nums) {
-        
         int n=nums.size();
-        vector<int>dp(n,-1);
-        int totalamount =robfrom(0,nums,dp,n);
-        return totalamount;   //return robfrom(0,nums,dp,n);
-    }
-    
-    
+        vector<int>dp(n+2,-1);
+        
+        dp[n]=0;
+        dp[n+1]=0;
+        for(int i=n-1;i>=0;i--){
+            dp[i]=max(dp[i],dp[i+1]);
+            dp[i]=max(dp[i],nums[i]+dp[i+2]);
+       }
+        return dp[0];
+    }  
 };
