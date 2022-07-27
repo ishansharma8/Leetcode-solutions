d329755a504f3f3111d9eddb66f42df3bd0e0611
Ishan-sharma8 <80027966+Ishan-sharma8@users.noreply.github.com>
2022-07-27 17:43:35 +0530

    Time: 887 ms (5.09%), Space: 67.7 MB (90.51%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -3,6 +3,7 @@ public:
     int maxSubArray(vector<int>& nums) {
         for(int i=1;i<nums.size();i++){
             nums[i]=max(nums[i] , nums[i-1]+nums[i]);
+            cout<<nums[i]<<" ";
         }
         int ans=*max_element(nums.begin(),nums.end());
         return ans;
