1db97d7b113ba323d14f2681d8a9c87350cb6531
Ishan-sharma8 <80027966+Ishan-sharma8@users.noreply.github.com>
2022-07-29 10:16:08 +0530

    Time: 123 ms (8.09%), Space: 59.6 MB (5.79%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
@@ -6,14 +6,16 @@ public:
         while(l<=r){
             mid=l+(r-l)/2;
             
-            if(mid!=0 && arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]){
-                return mid;
-            }
-            else if(arr[mid]<arr[mid+1]){  //lies in increasing sequence
+             if(arr[mid]<arr[mid+1]){  //lies in increasing sequence
                 l=mid+1;
                 }
+            
+            else if(arr[mid]<arr[mid-1]){
+                r=mid-1;
+            }
+            
             else{
-                r=mid-1;    //arr[mid]<arr[mid-1];
+                return mid;    //
             }
         }
         return mid;
