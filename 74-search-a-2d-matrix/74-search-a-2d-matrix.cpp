95373c3ceeac193b5ec0900ceb2338e55f10969f
Ishan-sharma8 <80027966+Ishan-sharma8@users.noreply.github.com>
2022-07-29 15:42:18 +0530

    Time: 3 ms (91.42%), Space: 9.5 MB (78.69%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -4,17 +4,32 @@ public:
         int m=matrix.size();//rows
         int n=matrix[0].size();//cols
         
-        int elem;
-        int row=0;
         
-        for(int i=0;i<m;i++){
-            elem=matrix[i][n-1];
-            if(target<=elem){
-                row=i;
-                break;
+        int low=0;
+        int high=m-1;
+        while(low<=high){
+            int middle=(low+high)/2;
+            int firstelem=matrix[middle][0];//col no fixed last col
+            
+            if(firstelem==target){
+                
+                return true;
+            }
+            else if(target<firstelem){
+                high=middle-1;
+                //cout<<high;
+            }
+            else {  //target>firstelem
+                low=middle+1;                                                   
             }
         }
-
+            
+            int row=high;
+            if(row<0){
+                return false;
+            }
+            
+            
             int l=0;
             int h=n-1;
             while(l<=h){
