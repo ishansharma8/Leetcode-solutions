687edaed415df07d1c90b302c05c76077874ef69
Ishan-sharma8 <80027966+Ishan-sharma8@users.noreply.github.com>
2022-08-03 12:59:10 +0530

    Time: 237 ms (74.22%), Space: 81.4 MB (52.23%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -1,88 +1,55 @@
 class Solution {
 public:
-    int largestRectangleArea(vector<int>& heights) {
-        
-        
-        int ans;
-        int finalans=0;
-        
-        int n=heights.size();
-        if(n==1){
-            return heights[0];
-        }
-        
-          
-        stack<int>s; 
-        
+    int largestRectangleArea(vector<int>& nums) {
+        int n=nums.size();
         vector<int>nsr(n,-1);
-               
-        for(int i=n-1;i>=0;i--){
-            while(!s.empty() && heights[s.top()]>=heights[i]){
-                s.pop();
-            }
-            
-            if(!s.empty()){
-                nsr[i]=s.top();
-            }
-            
-            s.push(i);
-        }
-        // cout<<nsr[0]<<endl;// cout<<nsr[1]<<endl;// cout<<nsr[2]<<endl;// cout<<nsr[3]<<endl;
-        // cout<<nsr[4]<<endl;// cout<<nsr[5]<<endl;
-        
-        
-        
-        
-        
-          
-        stack<int>s1; 
-        
         vector<int>nsl(n,-1);
-               
-        for(int i=0;i<n;i++){
-            while(!s1.empty() && heights[s1.top()]>=heights[i]){
+        int ans=0;
+        
+        stack<int>s1;
+        for(int i=n-1;i>=0;i--){
+            while(!s1.empty() && nums[s1.top()]>=nums[i]){
                 s1.pop();
             }
-            
             if(!s1.empty()){
-                nsl[i]=s1.top();
+                nsr[i]=s1.top();
             }
-            
             s1.push(i);
-        }  
+        }
         
-        // cout<<nsl[0]<<endl;// cout<<nsl[1]<<endl;// cout<<nsl[2]<<endl;// cout<<nsl[3]<<endl;
-        // cout<<nsl[4]<<endl;// cout<<nsl[5]<<endl;
-        
-        
-        
-        int wl=0;
-        int wr=0;
+        stack<int>s2;
+        for(int i=0;i<n;i++){
+            while(!s2.empty() && nums[s2.top()]>=nums[i]){
+                s2.pop();
+            }
+            if(!s2.empty()){
+                nsl[i]=s2.top();
+            }
+            s2.push(i);
+        }
+      
+        int wl=0,wr=0;
         for(int i=0;i<n;i++){
             if(nsl[i]==-1){
                 wl=i;
             }
+            
             else{
                 wl=i-nsl[i]-1;
             }
             
             if(nsr[i]==-1){
-                wr=n-i-1;
+                wr=(n-1)-i;
             }
+            
             else{
                 wr=nsr[i]-i-1;
             }
-            
-           // cout<<wl<<" "<<wr<<endl; this line
-            
-            ans=(wl+wr+1)*heights[i];
-            finalans=max(ans,finalans);
-            
-            finalans=max(ans,finalans);
-            
+            int width=(wl+wr+1)*nums[i];
+            ans=max(ans,width);
         }
-       
-    return finalans;    
         
+        return ans;
+            
     }
 };
