f205b249898beed3cffe068c8e8361d3c2bbf0ac
Ishan-sharma8 <80027966+Ishan-sharma8@users.noreply.github.com>
2022-08-12 20:20:10 +0530

    Time: 578 ms (7.32%), Space: 134.2 MB (8.39%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.cpp
+++ 912-sort-an-array/912-sort-an-array.cpp
@@ -2,35 +2,60 @@ class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
         int n=nums.size();
-        quicksort(nums,0,n-1);
+        mergesort(nums,0,n-1);
         return nums;
     }
     
-    void quicksort(vector<int>& nums,int st,int end){
-        //subarray defined by starting nd ending index
-        if(st>=end){
+    void mergesort(vector<int>& nums,int l,int r){
+        if(l>=r){
             return;
-        } 
-        int pivot_index=partition(nums,st,end);
-        quicksort(nums,st,pivot_index-1); 
-        quicksort(nums,pivot_index+1,end); 
-    }
-    
-    int partition(vector<int>& nums,int st,int end){
-        int random=st+rand()%(end-st);
-        swap(nums[end],nums[random]);
-        int pivot=nums[end];
-        int j=st-1;
-        for(int i=st;i<end;i++){
-            if(nums[i]<pivot){
-                j++;
-            swap(nums[j],nums[i]);
         }
+        int mid=l+(r-l)/2;
+        mergesort(nums,l,mid);
+        mergesort(nums,mid+1,r);
+        merge(nums,l,mid,r);
     }
-        j++;
-        swap(nums[j],nums[end]); //all nos less than last elemnt are upto j
-        return j;
+    void merge(vector<int>& nums,int l,int mid,int r){
+        vector<int>tempL,tempR;
+    
+        
+        for(int i=l;i<=mid;i++){
+            tempL.push_back(nums[i]);
+        }
+        
+        for(int i=mid+1;i<=r;i++){
+            tempR.push_back(nums[i]);
+        }
+        int left=0;
+        int right=0;
+        int ptr=l;
+        
+        while(left<tempL.size() && right<tempR.size() ){
+            if(tempL[left]<tempR[right]){
+                nums[ptr]=tempL[left];
+                left++;
+                ptr++;
+            }
+            else{
+                nums[ptr]=tempR[right];
+                right++;
+                ptr++;
+            }
+        }
+        
+        while(left<tempL.size()){
+            nums[ptr]=tempL[left];
+            left++;
+            ptr++;
+        }
+        while(right<tempR.size()){
+            nums[ptr]=tempR[right];
+            right++;
+            ptr++;
+        }
         
     }
-    
+        
+        
 };
+    
