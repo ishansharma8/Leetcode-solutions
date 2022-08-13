f860da588fed00425c4cf63863e0e05ea6cbec0b
Ishan-sharma8 <80027966+Ishan-sharma8@users.noreply.github.com>
2022-08-13 16:36:02 +0530

    Time: 108 ms (51.13%), Space: 28.5 MB (81.91%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.cpp
+++ 912-sort-an-array/912-sort-an-array.cpp
@@ -2,60 +2,34 @@ class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
         int n=nums.size();
-        mergesort(nums,0,n-1);
+        quicksort(nums,0,n-1);
         return nums;
     }
     
-    void mergesort(vector<int>& nums,int l,int r){
-        if(l>=r){
+    void quicksort(vector<int>& nums,int start ,int end){
+        if(start>=end){
             return;
         }
-        int mid=l+(r-l)/2;
-        mergesort(nums,l,mid);
-        mergesort(nums,mid+1,r);
-        merge(nums,l,mid,r);
+        int pivot_index=partition(nums,start,end);
+        quicksort(nums,start,pivot_index-1); 
+        quicksort(nums,pivot_index+1,end);     
     }
-    void merge(vector<int>& nums,int l,int mid,int r){
-        vector<int>tempL,tempR;
     
-        
-        for(int i=l;i<=mid;i++){
-            tempL.push_back(nums[i]);
-        }
-        
-        for(int i=mid+1;i<=r;i++){
-            tempR.push_back(nums[i]);
-        }
-        int left=0;
-        int right=0;
-        int ptr=l;
-        
-        while(left<tempL.size() && right<tempR.size() ){
-            if(tempL[left]<tempR[right]){
-                nums[ptr]=tempL[left];
-                left++;
-                ptr++;
-            }
-            else{
-                nums[ptr]=tempR[right];
-                right++;
-                ptr++;
+    int partition(vector<int>& nums,int start,int end){
+        int random=start+rand()%(end-start);
+        swap(nums[end],nums[random]);
+        int pivot=nums[end];
+        int j=start-1;
+        for(int i=start;i<end;i++){
+            if(nums[i]<pivot){
+                j++;
+                swap(nums[j],nums[i]);
+                
             }
         }
-        
-        while(left<tempL.size()){
-            nums[ptr]=tempL[left];
-            left++;
-            ptr++;
-        }
-        while(right<tempR.size()){
-            nums[ptr]=tempR[right];
-            right++;
-            ptr++;
-        }
-        
+        j++;
+        swap(nums[j],nums[end]);
+        return j;
     }
-        
-        
+    
 };
-    
