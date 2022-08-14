63fd4a59530a50cb4f64b42a1147f28cfa5fa0ac
Ishan-sharma8 <80027966+Ishan-sharma8@users.noreply.github.com>
2022-08-14 14:52:14 +0530

    Time: 1465 ms (8.33%), Space: 18.1 MB (29.07%) - LeetHub

--- 395-longest-substring-with-at-least-k-repeating-characters/395-longest-substring-with-at-least-k-repeating-characters.cpp
+++ 395-longest-substring-with-at-least-k-repeating-characters/395-longest-substring-with-at-least-k-repeating-characters.cpp
@@ -8,28 +8,25 @@ public:
     }
     
     int helper(string &s,int begin,int end,int k){
-        if((end-begin+1)<k)
-        {
-            return 0;
-        }
+        // if((end-begin+1)<k)
+        // {
+        //     return 0;
+        // }
       
         if(end<begin){
             return 0;//in case of 1st char j-1=0-1=-1    start=0,end=-1
         }
         
         cout<<begin<<" "<<end<<endl;
-        vector<int>v(26,0);
-        for(int i=begin; i<=end; i++)
-        {
-            int index = s[i] - 'a';
-            v[index]++;
-            
+        unordered_map<char,int>mp;
+        for(int i=begin;i<=end;i++){
+            mp[s[i]]++;  
         }
         
        // for(auto i:mp){cout<<i.first<<" "<<i.second<<endl;}
         int ans=end-begin+1;
         for(int j=begin;j<=end;j++){
-            if(v[s[j]- 'a']<k){
+            if(mp[s[j]]<k){
                 int left=helper(s,begin,j-1,k);
                 int right=helper(s,j+1,end,k);
                 ans=max(left,right);
