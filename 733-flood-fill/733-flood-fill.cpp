2bb81fec4f4b87d0338803bbc550e31660166a3b
Ishan-sharma8 <80027966+Ishan-sharma8@users.noreply.github.com>
2022-08-29 14:20:16 +0530

    Time: 16 ms (48.06%), Space: 14.5 MB (11.30%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,40 +1,36 @@
- class Solution {
+class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+      int n=image.size();
+      int m=image[0].size();
+      queue<pair<int,int>>q;
+      vector<vector<bool>>vis(n,vector<bool>(m,0));
+      q.push({sr,sc});
+      
+      while(!q.empty()){
+        pair<int,int>p=q.front();  //similar to int x =q.front();
+        // cout<<p.first<<" "<<p.second<<endl;
         
-       // bool visited[n][m]=false;
-        int n=image.size();
-        int m=image[0].size();
-        queue<pair<int,int>>q;
-        vector<vector<bool>>visited(n,vector<bool>(m,0));
+        q.pop();
+        int x=p.first;  //dont write p.first();
+        int y=p.second;  
+        vis[x][y]=1;
         
-        q.push({sr,sc});
-        
-        while(!q.empty()){
-            pair<int,int>p=q.front();
-            q.pop();
-           
-            int x=p.first;
-            int y=p.second;
-            visited[x][y]=1;
-         
-            int left[4]={1,0,-1,0};
-            int right[4]={0,1,0,-1};
-            for(int i=0;i<4;i++){
-            int row=left[i]+x;
-            int col=right[i]+y;
-            
-    if(row>=0 && col>=0 && row<n && col<m && visited[row][col]==0 && image[row][col]==image[x][y]){
-                q.push({row,col});
-                visited[row][col]=1;
-            }
-                cout<<q.size()<<" ";
-            }
-                
-                image[x][y]=color;
+        int horizontal[4]={1,0,-1,0};
+        int vertical[4]  ={0,1,0,-1};
+        for(int i=0;i<4;i++){
+          
+          int row=x+horizontal[i];
+          int col=y+vertical[i];
+          
+          if(row>=0 && col>=0 && row<n && col<m && vis[row][col]==0 && image[row][col]==image[x][y]){
+            q.push({row,col});
+            vis[row][col]=1;
+          }
         }
-    
-         return image;
-        
+        image[x][y]=color;
+      }
+      
+      return image;
     }
 };
