aea4179a379dccf893e457c229ba0585e6d6143b
Ishan-sharma8 <80027966+Ishan-sharma8@users.noreply.github.com>
2022-08-31 10:27:52 +0530

    Time: 12 ms (73.44%), Space: 10.3 MB (93.62%) - LeetHub

--- 841-keys-and-rooms/841-keys-and-rooms.cpp
+++ 841-keys-and-rooms/841-keys-and-rooms.cpp
@@ -19,7 +19,7 @@ public:
         for(int i=0;i<n;i++){
             if(vis[i]==0){
                 ans=0;
-              //  break;
+                break;
             }
         }
         return ans;
