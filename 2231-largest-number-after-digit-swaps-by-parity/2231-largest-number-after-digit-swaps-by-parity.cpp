c93dc107905fcf4ba6c6655b85827e413dc51663
Ishan-sharma8 <80027966+Ishan-sharma8@users.noreply.github.com>
2022-09-02 10:30:23 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (40.07%) - LeetHub

--- 2231-largest-number-after-digit-swaps-by-parity/2231-largest-number-after-digit-swaps-by-parity.cpp
+++ 2231-largest-number-after-digit-swaps-by-parity/2231-largest-number-after-digit-swaps-by-parity.cpp
@@ -7,7 +7,7 @@ public:
     string s=to_string(num);
         
         for(int i=0;i<s.size();i++){
-            int currno=s[i]-'0';
+            int currno=s[i];
             if(currno%2==0)
                 even.push(currno);
             else
@@ -15,14 +15,14 @@ public:
         }
         
         for(int i=0;i<s.size();i++){
-            int currno=s[i]-'0';
+            int currno=s[i];
             if(currno%2==0){
-                 s[i]=even.top()+'0';
+                 s[i]=even.top();
                  even.pop();
             }
                
             else{
-                s[i]=odd.top()+'0';  
+                s[i]=odd.top();  //or do +48
                 odd.pop();
             }
         }
