1460dd56d057d1e8f2180e82767f38e1a81dd30f
Ishan-sharma8 <80027966+Ishan-sharma8@users.noreply.github.com>
2022-09-11 21:45:50 +0530

    Time: 116 ms (6.09%), Space: 42.9 MB (5.00%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
@@ -11,20 +11,21 @@
  */
 class Solution {
 public:
-    // void inorder(TreeNode* root,unordered_set<int>&s){
-    //     if(root==NULL)return ;
-    //     inorder(root->left,s);
-    //     s.insert(root->val);
-    //     inorder(root->right,s);
-    // }
+    void inorder(TreeNode* root,unordered_set<int>&s){
+        if(root==NULL)return ;
+        inorder(root->left,s);
+        s.insert(root->val);
+        inorder(root->right,s);
+    }
     
     void traversetree(TreeNode* &t,unordered_set<int>&s,int k,bool &ans){
         //int k is just an integer so u can pass by ref,though u can pass by value also
         if(t==NULL)return;
         
-        s.insert(t->val);
+        //s.insert(t->val);
         if(s.find(k-t->val)!=s.end() && (t->val)*2!=k)
             ans=true;
+        
     
         traversetree(t->left,s,k,ans);
         traversetree(t->right,s,k,ans);
@@ -33,7 +34,7 @@ public:
     
     bool findTarget(TreeNode* root, int k) {
         unordered_set<int>s;
-        // inorder(root,s);
+        inorder(root,s);
         
         bool ans=false;
         traversetree(root,s,k,ans);
