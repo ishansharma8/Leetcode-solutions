95d5d612acdbbeae38703d8e267867628f1cf28e
Ishan-sharma8 <80027966+Ishan-sharma8@users.noreply.github.com>
2022-09-16 13:33:02 +0530

    Time: 111 ms (9.53%), Space: 19.1 MB (68.32%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        vector<vector<int>>res;
-        if(intervals.size()==0){
-            return res;
+      vector<vector<int>> output;
+      int n=intervals.size();
+      sort(intervals.begin(),intervals.end());
+      output.push_back(intervals[0]);
+      int prev_last,curr_start;
+      
+      for(int i=1;i<n;i++){
+        curr_start=intervals[i][0];
+        prev_last=output[output.size()-1][1];  //bcoz we want the last element of output ,if i=2 ,it need not be at output[1][1] ,it can be output[0][1]
+         cout<<i;
+
+        //cout<<curr_start<<prev_last;
+        
+        if(curr_start<=prev_last){
+          output[output.size()-1][1]=max(prev_last,intervals[i][1]);
         }
-        sort(intervals.begin(),intervals.end());
-        vector<int>curr;
-        curr=intervals[0];//this curr is a vector
-            
-            for(int i=1;i<intervals.size();i++){
-            if(curr[1]<intervals[i][0]){
-                res.push_back(curr);
-                curr=intervals[i];
-            }
-                else{
-                    curr[1]=max(curr[1],intervals[i][1]);
-                }
-                
-         }
-        res.push_back(curr);
-        return res;
+        
+        else{
+          output.push_back(intervals[i]);
+          // {intervals[i][0],intervals[i][1]}
+        }
+      }
+      
+      return output;
+        
     }
 };
