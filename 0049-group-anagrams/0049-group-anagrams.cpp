e3f71dbcdfe22eeb6759c625ccac7aa3d95ae66c
Ishan-sharma8 <80027966+Ishan-sharma8@users.noreply.github.com>
2022-10-28 20:34:39 +0530

    Time: 33 ms (97.27%), Space: 20.8 MB (34.92%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+      ios_base::sync_with_stdio(false);
+      cin.tie(NULL);
+      
+      
+      unordered_map<string,vector<string>>ump;
+      int n=strs.size();
+      string currstring;
+      
+      for(int i=0;i<n;i++){
+        currstring=strs[i];
+        sort(strs[i].begin(),strs[i].end());
+        ump[strs[i]].push_back(currstring);   
+        //each of these strs[i] acts a vector
+      }
+      
+      vector<vector<string>>ans;
+      for(auto x:ump){  
+        ans.push_back(x.second);
+      }
+      return ans;
+      
+    }
+};
