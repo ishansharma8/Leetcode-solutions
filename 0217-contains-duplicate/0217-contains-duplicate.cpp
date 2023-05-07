ac5c0179965f850be25dc60765ee776e7e465715
Ishan-sharma8 <80027966+Ishan-sharma8@users.noreply.github.com>
2023-05-07 12:51:40 +0530

    Time: 146 ms (49.77%), Space: 69.5 MB (50.51%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+    unordered_set<int>s;
+      for(auto elem:nums){
+        if(s.find(elem)!=s.end()){
+          return true;
+        }
+         s.insert(elem);
+      }
+      return false;
+    }
+};
