877d736a2d7007cc4bdfd12c79fa01ec194dc065
Ishan_4 <68964460+ishansharma8@users.noreply.github.com>
2024-03-06 02:17:37 +0530

    Time: 15 ms (70.83%), Space: 21.6 MB (56.36%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -4,28 +4,24 @@ public:
         int n=nums.size();
         int count0=0;
         
-        for(int i=0;i<n;i++){
-            if(nums[i]==0){
+        for(int k=0;k<n;k++){
+            if(nums[k]==0){
                 count0++;
             }
         }
         
         
-        vector<int>ans(n-count0,-1);
         int j=0;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++)
+        {
             if(nums[i]!=0){
-                ans[j]=nums[i];
+                nums[j]=nums[i];
                 j++;
             }
         }
-        for(int t=n-1;t>0;t--){
-            nums[t]=0;
-        }
         
-        for(int i=0;i<n-count0;i++){
-            nums[i]=ans[i];
+        for(int k=n-1;k>=n-count0;k--){
+            nums[k]=0;
         }
-        
     }
 };
