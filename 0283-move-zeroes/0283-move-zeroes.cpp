59cd06a44bcff7fb55de888d67fbfda6876edd84
Ishan_4 <68964460+ishansharma8@users.noreply.github.com>
2024-03-06 02:25:07 +0530

    Time: 12 ms (83.61%), Space: 21.6 MB (56.36%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -3,6 +3,7 @@ public:
     void moveZeroes(vector<int>& nums) {
         int n=nums.size();
         int count0=0;
+        int e=n-1;
         
         for(int k=0;k<n;k++){
             if(nums[k]==0){
@@ -20,8 +21,10 @@ public:
             }
         }
         
-        for(int k=n-1;k>=n-count0;k--){
-            nums[k]=0;
+        cout<<count0;
+        for(int k=0;k<count0;k++){
+            nums[e]=0;
+            e--;
         }
     }
 };
